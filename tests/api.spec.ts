b43dc0fd1413e102d98b8feaf336fcfbb9ed891e
Thiago <thiagocavalsilva@gmail.com>
2025-01-21 15:17:38 -0300

    Consulta Usuarios

---
+++ tests/api.spec.ts
@@ -0,0 +1,41 @@
+import { test, expect } from "@playwright/test";
+
+let authorization: any;
+let id: any;
+const API_URL = process.env.API_URL;
+
+test.describe("Suite de testes API ServRest", async () => {
+  
+  test.beforeEach("Before Each Hook - POST /login", async ({ request }) => {
+    console.log(API_URL);
+    const response = await request.post(`${API_URL}/login`, {
+      headers: {
+        "Content-Type": "application/json",
+      },
+      data: {
+        email: "fulano@qa.com",
+        password: "teste",
+      },
+    });
+    expect(response.status()).toBe(200);
+    let responseBody = await response.json();
+    authorization = responseBody.authorization;
+    console.log(responseBody)
+    return authorization;
+
+  });
+  
+  test("GET /usuarios", async ({ request }) => {
+    const response = await request.get(`${API_URL}/usuarios`);
+
+    console.log(response)
+    let responseStatus = await response.status();
+    let responseBody = await response.json();
+
+    console.log(responseStatus)
+    console.log(responseBody)
+    expect(responseStatus).toBe(200);
+ 
+  });
+
+});
